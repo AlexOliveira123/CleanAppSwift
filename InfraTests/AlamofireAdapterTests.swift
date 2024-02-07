c9ebf89ea887790ff6259e61507a1b71e97c3a6f
Alex Oliveira <allex.c.o@hotmail.com>
2024-02-06 22:17:06 -0300

    add Infra layer

---
+++ InfraTests/AlamofireAdapterTests.swift
@@ -0,0 +1,14 @@
+//
+//  InfraTests.swift
+//  InfraTests
+//
+//  Created by Alex Oliveira on 06/02/24.
+//
+
+import XCTest
+
+class AlamofireAdapterTests: XCTestCase {
+    func test_() {
+        
+    }
+}
